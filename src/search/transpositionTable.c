ad9aac00833c056597af91386d032a2b5699518a
Bautista Rodriguez <bautistarodriguezdl@gmail.com>
2024-02-25 00:03:26 -0300

    update 25-2-2024

--- src/search/transpositionTable.c
+++ src/search/transpositionTable.c
@@ -13,5 +13,23 @@ void initKeys()
 
 generateHashKey(struct BoardState board)
 {
-
+    U64 bitboard, finalKey = 0ULL;
+    for(int i = 0;i < 12;i++)
+    {
+        bitboard = board.bitboards[i];
+        while(bitboard)
+        {
+            int square = bitScan(bitboard);
+            finalKey ^= pieceKeys[i][square];
+            bitboard = popLSB(bitboard);
+        }
+    }
+    if(board.enPassant != 65)
+    {
+        int enPassantFile = (board.enPassant % 8);
+        finalKey ^= enPassantKeys[enPassantFile];
+    }
+    if(board.sideToMove == 1)
+        finalKey ^= sideKey;
+    return;
 }
