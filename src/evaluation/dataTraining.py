c329eca4ce98aa9f653cd6d0134aa57246a77040
Bautista Rodriguez <bautistarodriguezdl@gmail.com>
2024-03-08 21:50:46 -0300

    upd eval.py: readJSON - parseFEN

--- src/evaluation/dataTraining.py
+++ src/evaluation/dataTraining.py
@@ -1,8 +1,9 @@
 #20,999,266 chess positions evaluated with Stockfish. (Lichess Database)
-
+'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 
 class Model(nn.Module):
     def __init__(self,input = 790,h1 = 512,h2 = 128,h3 = 32,h4 = 32,output = 1):
@@ -20,3 +21,21 @@ class Model(nn.Module):
         x = F.relu(self.fc5(x))
         x = self.out(x)
         return x
+'''
+import json
+import chess
+
+board = chess.Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+
+
+
+with open('C:/Users/bauty/Desktop/Bau/ChessNNDataset/lichess_db_eval.jsonl', 'r') as dataSet:
+    for i, line in enumerate(dataSet):
+        if i == 3:
+            break
+        strFile = json.loads(line)
+        WP = board.pieces(chess.PAWN, chess.WHITE)
+        print(int(WP))
+        board.set_fen(strFile['fen'])
+        cp = strFile['evals'][0]['pvs'][0]['cp']
+        print(strFile['fen'],"| cp:",cp)
